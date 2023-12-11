fadaa2f89df94aed356580f7265499c84f4ca601
Vitor Garcez <vitaoog@gmail.com>
2023-12-11 20:12:47 -0300

    Commit inicial da atividade

---
+++ src/connect_database.py
@@ -0,0 +1,23 @@
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+import json
+
+cloud_config= {
+  'secure_connect_bundle': 'secure-connect-mercado-livre.zip'
+}
+
+with open("vitor.oliveira67@fatec.sp.gov.br-token.json") as f:
+    secrets = json.load(f)
+
+CLIENT_ID = secrets["clientId"]
+CLIENT_SECRET = secrets["secret"]
+
+auth_provider = PlainTextAuthProvider(CLIENT_ID, CLIENT_SECRET)
+cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
+session = cluster.connect()
+
+row = session.execute("select release_version from system.local").one()
+if row:
+  print(row[0])
+else:
+  print("An error occurred.")
